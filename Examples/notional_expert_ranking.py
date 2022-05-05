aed440edc780bc12186ceb6992e87ecd60b578c8
acox37 <adam.cox@asdl.gatech.edu>
2022-05-05 14:41:39 -0400

    feat(notional): start dividing up notional examples
    notional_models.py creates models, notional_expert_ranking.py
    shows how to perform expert opinion based scoring.

---
+++ Examples/notional_expert_ranking.py
@@ -0,0 +1,97 @@
+"""
+Create initial fidelity ranking based on expert opinion orders
+
+Contains multiple functions to depict different scenarios of expert
+model rankings in terms of resolution, abstraction, and scope to be
+combined
+"""
+from notional_models import *
+from FAMS.model_rankings import Ranking
+
+
+def no_scope():
+    """
+    Only resolution and abstraction, both purely increasing
+
+    Notes
+    -----
+    Orders should typically be added using Order from model_rankings
+    and Ranking.add_order, shortcut methods are used here for these
+    simplified cases
+
+    Returns
+    -------
+    individual_ranking
+    """
+    resolution = Ranking(models, 'Resolution')
+    resolution.add_order_increasing(models)
+    abstraction = Ranking(models, 'Abstraction')
+    abstraction.add_order_increasing(models)
+    return resolution, abstraction
+
+
+def fixed_scope():
+    """
+    Resolution, abstraction, and scope, with purely increasing
+    resolution and abstraction and fixed scope
+
+    Notes
+    -----
+    Orders should typically be added using Order from model_rankings
+    and Ranking.add_order, shortcut methods are used here for these
+    simplified cases
+
+    Returns
+    -------
+    individual_ranking
+    """
+    resolution = Ranking(models, 'Resolution')
+    resolution.add_order_increasing(models)
+    abstraction = Ranking(models, 'Abstraction')
+    abstraction.add_order_increasing(models)
+    scope = Ranking(models, 'Scope')
+    scope.add_order_fixed(models)
+    return resolution, abstraction, scope
+
+
+def decreasing_scope():
+    """
+    Resolution, abstraction, and scope, with purely increasing
+    resolution and abstraction and purely decreasing scope, representing
+    a realistic scenario where scope has to be decreased as resolution
+    and abstraction are improved
+
+    Notes
+    -----
+    Orders should typically be added using Order from model_rankings
+    and Ranking.add_order, shortcut methods are used here for these
+    simplified cases
+
+    Returns
+    -------
+    individual_ranking
+    """
+    resolution = Ranking(models, 'Resolution')
+    resolution.add_order_increasing(models)
+    abstraction = Ranking(models, 'Abstraction')
+    abstraction.add_order_increasing(models)
+    scope = Ranking(models, 'Scope')
+    scope.add_order_decreasing(models)
+    return resolution, abstraction, scope
+
+
+if __name__ == '__main__':
+    # combined = Ranking.combine(no_scope(), name='notional')
+    # combined = Ranking.combine(fixed_scope(), name='notional')
+    combined = Ranking.combine(decreasing_scope(), name='notional')
+    if plot:
+        # Plot fidelity KDE distributions
+        combined_models = combined.plot_scores(
+            cdf=cdf, cmap=cmap, title=False, legend=True, use_ids=True)
+        if tight:
+            plt.tight_layout()
+        if save:
+            plt.savefig('notional-dists.{}'.format(save))
+        # Plot stacked bar charts of fidelity probabilities
+        combined.plot_score_bars(cmap=cmap)
+    plt.show()
---
+++ Examples/notional_models.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from matplotlib.cm import gray, gray_r, viridis
+import random
+
+from FAMS.model_rankings import Model
+
+fig_width = 6.4  # matplotlib default
+fig_ar = 4 / 3  # default is 4 / 3
+fig_height = fig_width / fig_ar
+plt.rc('font', family='Times New Roman', size=12)
+plt.rc('figure', figsize=(fig_width, fig_height))
+plt.rc('savefig', dpi=600, transparent=True)
+
+cdf = False
+cmap, plot = viridis, True
+# dark_background, tight, save = True, True, 'svg'  # for presentation
+dark_background, tight, save = False, False, False  # for running and testing
+# dark_background, tight, save = False, True, 'pdf'  # for document
+threshold, freq_only = 80, False
+if dark_background:
+    mono_cmap = gray_r
+    plt.rc(('xtick', 'ytick'), color='w')
+    plt.rc('text', color='w')
+    plt.rc('axes', edgecolor='w', labelcolor='w')
+    plt.rc('legend', framealpha=0.)
+else:
+    mono_cmap = gray
+
+random.seed(0)  # for randomly generating orders if needed
+
+num_models = 4
+
+models = [Model() for _ in range(num_models)]
