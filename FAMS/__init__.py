b3ea84f6526bd6acb67f3a624da8ff1758679b01
acox37 <acox37@gatech.edu>
2023-10-29 13:33:13 -0400

    minor: add __init__.py, TODOs

---
+++ FAMS/__init__.py
@@ -0,0 +1,17 @@
+"""
+TODO
+----
+- Split Ranking and CombinedRanking?
+  - Ability to look at probabilities based on one fidelity metric
+  - Clarification since now two different types of Ranking
+- MultiRanking: Look at multiple responses
+  - MultiRanking would be multiple generic metrics
+  - Combined ModelRanking still multi-attribute since response + cost
+  - MultiModelRanking would be multiple outputs + cost
+- Portfolios
+  - Can look at model portfolios with ModelRanking and MultiModelRanking
+  - MultiModelRanking would just be different scoring method
+  - For generic items, could look at portfolios for Ranking, but might
+    just be additive (potentially with compatibility matrix)
+  - MultiRanking would involve MADM/TOPSIS and be more interesting
+"""
