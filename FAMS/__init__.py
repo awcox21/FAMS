da729f2df73a7168720046bbb14c59a42d7e3b08
acox37 <acox37@gatech.edu>
2023-11-27 14:34:27 -0500

    docs: Add future work notes to __init__

--- FAMS/__init__.py
+++ FAMS/__init__.py
@@ -8,10 +8,13 @@ TODO
   - MultiRanking would be multiple generic metrics
   - Combined ModelRanking still multi-attribute since response + cost
   - MultiModelRanking would be multiple outputs + cost
+  - For models, this can also be WRT multiple responses
 - Portfolios
   - Can look at model portfolios with ModelRanking and MultiModelRanking
   - MultiModelRanking would just be different scoring method
   - For generic items, could look at portfolios for Ranking, but might
     just be additive (potentially with compatibility matrix)
   - MultiRanking would involve MADM/TOPSIS and be more interesting
+- Add setup and other package files
+- Add unit tests
 """
